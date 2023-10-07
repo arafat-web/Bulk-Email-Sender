ee45df4b1fb5276fdf7be99c86b42745c1c4680b
Arafat Hossain Ar <arafat.122260@gmail.com>
2023-10-08 01:00:26 +0600

    admin seeder added

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('admins')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
